1330ea6fed5b3292e61764da309bb295dca21e9e
ひろろ <hrsi.ad@gmail.com>
2022-06-26 10:11:05 +0900

    import 勉強

---
+++ get_math.py
@@ -0,0 +1,15 @@
+
+
+import math
+
+def get_trialgle(base, height):
+  return base * height / 2
+
+def get_circle(radius):
+  return radius * radius * math.pi
+
+
+
+print('-'*10 + f'{__name__}' + '-'*10)
+print(f'__name__ は {__name__}となっている。')
+
---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+  print(f'__name__ は {__name__}となっている。')
+  print(f'三角形の面積は {get_trialgle(10,2)}')
+  print(f'円の面積は {get_circle(10,)}')
+
